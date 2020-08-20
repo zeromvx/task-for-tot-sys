4614ef3dac0823fbc73439502702d98f66931e60
Sergey Karmanov <zeromvx@gmail.com>
2020-08-20 12:50:48 +0300

    Create base components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Drawer from "./components/Drawer/Drawer";
+import ChatWindow from "./components/ChatWindow/ChatWindow";
 
 function App() {
   return (
     <div className="App">
-
+      <Drawer />
+      <ChatWindow />
     </div>
   );
 }
