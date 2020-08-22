c2926e16c380bbeddd544c5c7ad660ed46051773
Sergey Karmanov <zeromvx@gmail.com>
2020-08-22 09:18:42 +0300

    make it better

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const favicon = require('express-favicon');
 
 const path = require('path');
 const port = 8080;
 
 const app = express();
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
